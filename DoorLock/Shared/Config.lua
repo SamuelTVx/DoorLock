ff7181a13229d81bc6ae48af589391f9750d18c8
SamuelTVx <121121539+SamuelTVx@users.noreply.github.com>
2022-12-20 18:30:52 +0100

    Add files via upload

---
+++ DoorLock/Shared/Config.lua
@@ -0,0 +1,23 @@
+Config = {} or Config
+
+Config['usingOldESX'] = true
+
+Config['enableNuiIndicator'] = true
+
+Config['scriptVersion'] = 1.4
+
+Config['admingroups'] = {
+    "admin",
+    "superadmin",
+    "mod"
+}
+
+Config['commands'] = {
+    ['CreateDoor'] = "createdoor",
+    ['RemoveDoor'] = "deletedoor"
+}
+
+Config["strings"] = {
+    ['open'] = "[~r~E~w~] Unlock",
+    ['close'] = "[~g~E~w~] Lock",
+}
